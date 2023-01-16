413cbf2266ef99ef1dc755aaef31b7b007d06b6d
Roger Terrill <lpaddikt@gmail.com>
2023-01-15 17:38:05 -0800

    A-Fixed file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 import {HeaderComponent} from './header/header.component';
 import {RecipesComponent} from './recipes/recipes.component';
-import {RecipesListComponent} from './recipes/recipes-list/recipes-list.component';
-import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
-import { RecipesItemComponent } from './recipes/recipe-list/recipes-item/recipes-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
+import {RecipesDetailComponent} from './recipes/recipes-detail/recipes-detail.component';
+import {RecipesItemComponent} from './recipes/recipe-list/recipes-item/recipes-item.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipesListComponent,
+    RecipeListComponent,
     RecipesDetailComponent,
     RecipesItemComponent,
     ShoppingListComponent,
