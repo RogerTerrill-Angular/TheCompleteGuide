08989849db2cd4f0c0d5c9b12c1ce5ad1daf64a4
Roger Terrill <lpaddikt@gmail.com>
2023-02-23 11:06:22 -0800

    102-Closing the dropdown from anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -6,7 +6,14 @@ import {Directive, HostBinding, HostListener} from '@angular/core';
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleOpen() {
+  @HostListener('click') click() {
     this.isOpen = !this.isOpen;
+  }
+
+  @HostListener('focusout') focusout() {
+    this.isOpen = false;
+  }
+
+  constructor() {
   }
 }
